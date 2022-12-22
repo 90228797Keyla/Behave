14815c5c87eded7cf9681e3dde69dd3c1eff9bf6
90228797Keyla <keyla.rojas@coppel.com>
2022-12-22 17:46:42 -0600

    Se agrega segundo escenario para el feature login

--- environment.py
+++ environment.py
@@ -1,16 +1,23 @@
 from driver_interactions.ElementsInteractions import ElementsInteractions
 from driver_interactions.InitWebDriver import InitWebDriver
+import utilities.Logger as log
 import time
 
+log = log.func_logger()
 
 def before_all(context):
+    log.info("Script iniciado")
+
+def before_scenario(context, scenario):
     context.web_driver = InitWebDriver().init_web_driver()
     context.interactions_object = ElementsInteractions(context.web_driver)
-    context.interactions_object.launch_web_page("http://www.saucedemo.com")
+    context.interactions_object.launch_web_page("https://www.saucedemo.com")
 
+def after_scenario(context, scenario):
+    context.web_driver.quit()
 
 def after_all(context):
-    time.sleep(5)
+    time.sleep(15)
     context.web_driver.quit()
 
 
